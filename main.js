bb6de88247cd67cf34d589ca74345464c92bd34e
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-06-03 00:57:45 -0400

    pain

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let myLibrary = [];
 
+
 function Book(title,author,numPages,hasRead){
     this.title = title;
     this.author = author;
     this.numPages = numPages;
     this.hasRead = hasRead;
     
-
 }
 
 Book.prototype.read = function(){
@@ -20,6 +20,27 @@ function addBook(title,author,numPages,hasRead){
     myLibrary.push(newBook);
 }
 
-addBook('The Hobbit', 'J.R.R. Tolkien', 295, false);
+function formValidator(){
+    const inputs = document.querySelector('#addBookForm').getElementsByTagName("input");
+    let readTemp = false;
+
+    if(inputs[3].checked ==true){
+        readTemp = true;
+    }
+    addBook(inputs[0].value,inputs[1].value,inputs[2].value,readTemp);
+    console.log(myLibrary);
+
+}
+
+const addBookBtn = document.querySelector('#addBookBtn');
+addBookBtn.addEventListener('click',()=> formValidator());
+
+const bookGrid = document.querySelector('.cardGrid');
+
+function displayBooks(){
+    
+}
+
+//addBook('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
 console.log(myLibrary);
